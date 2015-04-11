14e26ed999d93feb4047645e865622b16ccfb346
Johan Poirier <johan.poirier@gmail.com>
2015-04-11 18:34:42 +0200

    Fix navigation

--- content/nav.js
+++ content/nav.js
@@ -1,10 +1,10 @@
 (function () {
-    var sections, indexSection = 0,
+    var sections, indexSection = 0, currentSection,
         main = window.document.querySelector("main");
 
     var changeSlide = function () {
         main.style.transform = "translateY(-" + (indexSection * window.document.body.offsetHeight) + "px)";
-        window.history.pushState({ topic: sections[indexSection].id }, null, "#" + sections[indexSection].id);
+        window.history.pushState({ topic: sections[indexSection].id }, null, "#!" + sections[indexSection].id);
     };
 
     var prevSlide = function () {
@@ -22,9 +22,23 @@
     };
 
     var startApp = function () {
+        var hashReg = /#\!(.*)$/,
+            topicId = hashReg.exec(window.location.href);
+
         sections = document.querySelectorAll("section");
         key('left, esc', prevSlide);
         key('right, space, enter', nextSlide);
+
+        if (topicId) {
+            currentSection = topicId[1];
+            for (var i = 0; i < sections.length; i++) {
+                if (sections[i].id === currentSection) {
+                    indexSection = i;
+                    changeSlide();
+                    break;
+                }
+            }
+        }
     };
 
     startApp();
